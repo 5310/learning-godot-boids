902669eea3df2822e3cbefe854206b3bb6ae2204
Scio <sayantan.chaudhuri@gmail.com>
2025-01-09 20:55:40 +0530

    Cleanup

--- boid.gd
+++ boid.gd
@@ -3,28 +3,25 @@ class_name Boid extends Area2D
 
 @onready var screen_rect = get_viewport_rect()
 
-var timescale = 60
-var patience: float = 1.0
-var speed: float = 1.0
-var range: int: 
-	get:
-		return $CollisionShape2D.shape.radius
-	set(value):
-		$CollisionShape2D.shape.radius
-var radius: float = 20
-var separation: float = 0.01
-var alignment: float = 0.01
-var cohesion: float = 0.01
+static var timescale = 60
+static var heat = 0.0
+static var speed: float = 1.0
+static var radius: float = 20
+static var range: float = 50
+static var separation: float = 0.01
+static var alignment: float = 0.01
+static var cohesion: float = 0.01
 
 
 func _ready() -> void:
-	range = 100
 	position = Vector2(randf() * screen_rect.size.x, randf() * screen_rect.size.y)
 	rotation = randf() * TAU
 
 
 func _process(delta: float) -> void:
-		
+	
+	$CollisionShape2D.shape.radius = max(radius, 10)
+	
 	var neighbors = get_overlapping_areas()
 	var cohesion_target = neighbors.reduce(func (sum, neighbor): return sum + neighbor.position, Vector2(0, 0))/(neighbors.size() if neighbors else 1) - position
 	var alignment_target = Vector2.RIGHT.rotated(neighbors.reduce(func (sum, neighbor): return sum + neighbor.rotation, 0)/(neighbors.size() if neighbors else 1))
@@ -33,12 +30,15 @@ func _process(delta: float) -> void:
 		var distance = neighbor.position - position
 		if distance.length() < radius:
 			separation_target -= distance.normalized() * (radius - distance.length())
+	var jitter = randf_range(-1, +1) * TAU * delta * (1 if randf() < heat else 0)
 	
 	var bearing = Vector2.RIGHT.rotated(rotation)
-	bearing = (bearing + (alignment_target * alignment + cohesion_target * cohesion + separation_target * separation) * delta * timescale).normalized()
+	bearing += (alignment_target * alignment + cohesion_target * cohesion + separation_target * separation) * delta * timescale
+	bearing = bearing.normalized()
 	
 	translate(bearing * speed * delta * timescale)
 	rotation = bearing.angle()
+	rotate(jitter)
 	
 	position.x = wrapf(position.x, screen_rect.position.x, screen_rect.end.x)
 	position.y = wrapf(position.y, screen_rect.position.y, screen_rect.end.y)
--- root.gd
+++ root.gd
@@ -1,24 +1,28 @@
 extends Node2D
 
 
-@export_range(20, 2000) var population: float = 200
+@export_range(20, 500) var population: int = 200
 var boid_scene = preload("res://boid.tscn")
 
+@export_range(0, 1) var heat: float = 0.1
 @export_range(0, 100) var speed: float = 1.0
-@export_range(0, 100) var range: int = 20
+@export_range(0, 100) var range: float = 50
 @export_range(0, 100) var radius: float = 20
 @export_range(0, 1) var separation: float = 0.01
 @export_range(0, 1) var alignment: float = 0.01
-@export_range(0, 1) var cohesion: float = 0.01
+@export_range(0, 1) var cohesion: float = 0.005
 
 
 func _ready() -> void:
 	for i in population: 
 		var boid = boid_scene.instantiate()
-		boid.speed = speed
-		boid.range = range
-		boid.radius = radius
-		boid.separation = separation
-		boid.alignment = alignment
-		boid.cohesion = cohesion
 		add_child(boid)
+
+func _process(delta: float) -> void:
+	Boid.heat = heat
+	Boid.speed = speed
+	Boid.range = range
+	Boid.radius = radius
+	Boid.separation = separation
+	Boid.alignment = alignment
+	Boid.cohesion = cohesion
